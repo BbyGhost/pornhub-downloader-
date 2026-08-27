f9071bd0a9cc7e32fbcb6592dd5d3d523b3af47d
BbyGhost <anuragbehera.behera@gmail.com>
2026-08-28 02:31:16 +0530

    Detect Eporner master playlists from resource entries

--- extension/page-hook.js
+++ extension/page-hook.js
@@ -1,4 +1,6 @@
 (() => {
+  "use strict";
+
   const emit = (url, mime = "") => {
     try {
       const u = new URL(url, location.href);
@@ -8,8 +10,32 @@
       }
     } catch {}
   };
+
+  const scanResources = () => {
+    try {
+      for (const e of performance.getEntriesByType("resource")) {
+        const u = e?.name || "";
+        if (/\.m3u8(?:[?#]|$)|\.mpd(?:[?#]|$)|master\.m3u8|\.urlset\//i.test(u)) emit(u);
+      }
+    } catch {}
+  };
+
   const oldFetch = window.fetch;
-  if (oldFetch) window.fetch = function(input, init){ try { emit(typeof input === "string" ? input : input?.url, ""); } catch {} return oldFetch.apply(this, arguments); };
+  if (oldFetch) {
+    window.fetch = function(input, init) {
+      try { emit(typeof input === "string" ? input : input?.url, ""); } catch {}
+      return oldFetch.apply(this, arguments);
+    };
+  }
+
   const oldOpen = XMLHttpRequest.prototype.open;
-  XMLHttpRequest.prototype.open = function(method, url){ try { emit(url, ""); } catch {} return oldOpen.apply(this, arguments); };
+  XMLHttpRequest.prototype.open = function(method, url) {
+    try { emit(url, ""); } catch {}
+    return oldOpen.apply(this, arguments);
+  };
+
+  scanResources();
+  setTimeout(scanResources, 500);
+  setTimeout(scanResources, 1500);
+  setInterval(scanResources, 3000);
 })();
