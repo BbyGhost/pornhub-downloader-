ba918c16d2e4a4586f3c0ba7628efc5ef5932d31
BbyGhost <anuragbehera.behera@gmail.com>
2026-08-28 16:49:42 +0530

    Add direct site buttons with favicons

--- extension/popup.js
+++ extension/popup.js
@@ -22,3 +22,32 @@ async function update(){
   setTimeout(()=>chrome.runtime.reload(),15000);
 }
 btn.onclick=check; check();
+
+const sites = [
+  ["XVideos","https://www.xvideos.com/"],
+  ["Pornhub","https://www.pornhub.com/"],
+  ["xHamster","https://xhamster.com/"],
+  ["XNXX","https://www.xnxx.com/"],
+  ["YouPorn","https://www.you-porn.com/"],
+  ["Eporner","https://www.eporner.com/"],
+  ["RedTube","https://www.redtube.com/"],
+  ["Sxyprn","https://www.sxyprn.com/"],
+  ["SpankBang","https://spankbang.com/"]
+];
+const sitesEl = document.getElementById("sites");
+if (sitesEl) {
+  for (const [name, url] of sites) {
+    const a = document.createElement("a");
+    a.className = "site";
+    a.href = url;
+    a.target = "_blank";
+    a.rel = "noopener";
+    const img = document.createElement("img");
+    img.alt = "";
+    img.src = "https://www.google.com/s2/favicons?domain=" + encodeURIComponent(new URL(url).hostname) + "&sz=64";
+    const span = document.createElement("span");
+    span.textContent = name;
+    a.append(img, span);
+    sitesEl.appendChild(a);
+  }
+}
